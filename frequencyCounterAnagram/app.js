63554d3cb877bcfdcb09f8a2538b8e4092b04206
megan <msmeganmcmanus@gmail.com>
2020-08-19 15:23:26 -0700

    add 3rd solution

--- frequencyCounterAnagram/app.js
+++ frequencyCounterAnagram/app.js
@@ -3,6 +3,7 @@
 // An anagram is a word, phrase, or name formed by rearranging the letters of
 //another, such as cinema, formed from iceman.
 
+//my solution
 function makeFreqCounter(str) {
 	const freqCounter = {};
 	for (let char of str) {
@@ -29,4 +30,26 @@ console.log(validAnagram('anagram', 'nagaram'));
 //Not using a Frequency counter and inefficient:
 function validAnagram2(str1, str2) {
 	return str1.split('').sort().join('') === str2.split('').sort().join('');
+}
+
+//---------------------------------------------------------------------
+//Using MAP // colts solution
+function makeFreqCounter3(str) {
+	let frequencies = new Map();
+	for (let val of str) {
+		let valCount = frequencies.get(val) || 0;
+		frequencies.set(val, valCount + 1);
+	}
+	return frequencies;
+}
+
+function validAnagram3(str1, str2) {
+	if (str1.length !== str2.length) return false;
+	const str1Freq = makeFreqCounter3(str1);
+	const str2Freq = makeFreqCounter3(str2);
+	if (str1Freq.size !== str2Freq.size) return false;
+	for (let letter of str1Freq.keys()) {
+		if (str2Freq.get(letter) !== str2Freq.get(letter)) return false;
+	}
+	return true;
 }
